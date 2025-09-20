ac9b2c4e0406eaa26250f1ded4bef714c2b3c3d8
CarloMart88 <carlo.martino1988@gmail.com>
2025-09-20 10:11:47 +0200

    feat: work on Html to prepare the axios call

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,9 +2,14 @@ import styles from "../modules/Header.module.css";
 
 function Header() {
   return (
-    <div className="container">
+    <div className="container my-3">
       <div className="row">
-        <div className="col-12">header</div>
+        <div className="col-12 text-center">
+          <h1>
+            <em>Welcome to my favourites movies library</em>
+          </h1>
+          <h3 className="my-2">Enjoy the vision</h3>
+        </div>
       </div>
     </div>
   );
--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -4,7 +4,20 @@ function HomePage() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-12">HomePage</div>
+        <div className="col-12">
+          <div className="card">
+            <div className="card-img">
+              <img src="" alt="" />
+            </div>
+            <div className="card-body">
+              <h3>title:</h3>
+              <h4>director:</h4>
+              <h4>genre:</h4>
+              <h4>relase year:</h4>
+              <div>overlay</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
