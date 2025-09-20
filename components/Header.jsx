6f2750868e10e7f2913f535ff712553597743309
CarloMart88 <carlo.martino1988@gmail.com>
2025-09-20 15:06:41 +0200

    feat: add some css classes and make it better

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,16 +2,16 @@ import styles from "../modules/Header.module.css";
 
 function Header() {
   return (
-    <div className="container my-3">
+    <header className="container my-3">
       <div className="row">
         <div className="col-12 text-center">
           <h1>
-            <em>Welcome to my favourites movies library</em>
+            <em>⭐📽️Welcome to my favourites movies library📽️⭐</em>
           </h1>
-          <h3 className="my-2">Enjoy the vision</h3>
+          <h3 className="my-2">🍿⭐Enjoy the vision⭐🍿</h3>
         </div>
       </div>
-    </div>
+    </header>
   );
 }
 
--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,21 +1,57 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+//creo la variabile per il data fetching che farò direttamente nello useEffect con la chiamata axios
 
 function HomePage() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/movies")
+      .then((resp) => {
+        setMovies(resp.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log(movies);
+
   return (
-    <div className="container">
+    <div className="container body">
       <div className="row">
-        <div className="col-12">
-          <div className="card">
-            <div className="card-img">
-              <img src="" alt="" />
-            </div>
-            <div className="card-body">
-              <h3>title:</h3>
-              <h4>director:</h4>
-              <h4>genre:</h4>
-              <h4>relase year:</h4>
-              <div>overlay</div>
-            </div>
+        <div className="col-12 attribute">
+          {movies.map((movie) => {
+            const {
+              id,
+              title,
+              director,
+              genre,
+              release_year,
+              image,
+              abstract,
+            } = movie;
+            return (
+              <div className="col-12 col-md-6 col-lg-4 card m-5" key={id}>
+                <div className="card-img">
+                  <img src={image} alt={title} />
+                </div>
+                <div className="overlay">
+                  <div className="card-body my-5">
+                    <h2>Title:{title} </h2>
+                    <h5>Director: {director} </h5>
+                    <h5>Genre: {genre}</h5>
+                    <h5>Relase: {release_year}</h5>
+                    <h6>Description: {abstract} </h6>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+          <div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
           </div>
         </div>
       </div>
