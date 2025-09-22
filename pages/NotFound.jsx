43c860c2699eee44ad64330d122df376897ad21e
CarloMart88 <carlo.martino1988@gmail.com>
2025-09-22 16:49:05 +0200

    feat: translate notFound

--- pages/NotFound.jsx
+++ pages/NotFound.jsx
@@ -3,11 +3,8 @@ import { Link } from "react-router-dom";
 function NotFound() {
   return (
     <div className="col-12 d-flex flex-column align-items-center justify-content-center my-5">
-      <h1>Pagina non trovata :( </h1>
-      <p className="my-3">
-        Ci dispiace la pagina cercata non esiste clicca sul pulsante per tornare
-        indietro
-      </p>
+      <h1>Page not found </h1>
+      <p className="my-3">We 're sorry but we couldn't find any films</p>
       <Link to="/">
         <button className="btn back-home mx-5 my-5">
           <i className="fa-regular fa-house" title="torna indietro"></i>
